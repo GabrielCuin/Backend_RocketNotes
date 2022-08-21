0b240bf9b7961d7eab033ee30446cad813506cbb
Gabriel <gabrielcuinprof@gmail.com>
2022-08-21 13:44:44 -0300

    PORT updated

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use((error, req, res, next) => {
   });
 });
 
-const PORT = process.env.SERVER_PORT || 3333;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
